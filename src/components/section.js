7593a8165429d68f29aa8a74a5ee15b77373f0e9
Josh Newton <josh.newton@me.com>
2019-07-06 19:08:07 -0400

    Adding newly added files for styles and updated content.

---
+++ src/components/section.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+import ScrollAnimation from 'react-animate-on-scroll'
+
+class Section extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <section id={this.props.id} className="container body">
+        <ScrollAnimation animateIn="fadeInRight">
+      		<h1>{this.props.sectionTitle}</h1>
+        </ ScrollAnimation>
+        <ScrollAnimation animateIn="fadeIn">
+      		<div className="wrapper">
+            {this.props.children}
+      		</div>
+        </ScrollAnimation>
+      </section>
+    )
+  }
+}
+
+export default Section
