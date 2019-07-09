52a2f0c2a0a1582467e6aa236b8acb8081e987d3
Josh Newton <josh.newton@me.com>
2019-07-08 22:48:16 -0400

    Update footer layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,23 +12,43 @@ class Footer extends Component {
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeIn" offset={1000}>
           <div className="wrapper">
-            <a href="https://facebook.com/newtonjd1" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://facebook.com/newtonjd1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FontAwesomeIcon icon={["fab", "facebook-square"]} />
               <i className="fa fa-facebook-square" aria-hidden="true"></i>
             </a>
-            <a href="https://twitter.com/imjoshnewton" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://twitter.com/imjoshnewton"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FontAwesomeIcon icon={["fab", "twitter-square"]} />
               <i className="fa fa-twitter-square" aria-hidden="true"></i>
             </a>
-            <a href="https://www.linkedin.com/in/newtonjosh" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.linkedin.com/in/newtonjosh"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FontAwesomeIcon icon={["fab", "linkedin"]} />
               <i className="fa fa-linkedin-square" aria-hidden="true"></i>
             </a>
-            <a href="https://instagram.com/imjoshnewton" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://instagram.com/imjoshnewton"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FontAwesomeIcon icon={["fab", "instagram"]} />
               <i className="fa fa-instagram" aria-hidden="true"></i>
             </a>
-            <a href="https://unsplash.com/@imjoshnewton" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://unsplash.com/@imjoshnewton"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FontAwesomeIcon icon="camera" />
               <i className="fa fa-camera" aria-hidden="true"></i>
             </a>
