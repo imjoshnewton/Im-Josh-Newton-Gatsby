8d34cf85cbf96a6895e82ef2dc615945f366dcd5
Josh Newton <josh.newton@me.com>
2020-07-30 14:57:07 -0400

    Update Favicon and Fix Errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,14 +80,14 @@ module.exports = {
       },
     },
     `gatsby-transformer-yaml`,
-    // {
-    //   resolve: `gatsby-source-remote-images`,
-    //   options: {
-    //     filter: node => node.internal.type === `UnsplashImagesYaml`,
-    //   },
-    // },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-remote-images`,
+      options: {
+        filter: node => node.internal.type === `UnsplashImagesYaml`,
+      },
+    },
+    this (optional) plugin enables Progressive Web App + Offline functionality
+    To learn more, visit: https://gatsby.dev/offline
+    `gatsby-plugin-offline`,
   ],
 }
